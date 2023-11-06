d4b623b5ceda2060355a55bb4099c57eb1dac072
Ranjit Raut <67329253+theranjitraut@users.noreply.github.com>
2023-11-06 18:31:27 +0545

    bugs are fixed

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-<script>
 let toggleBurger = document.getElementsByClassName('burger')[0];
 let toggleNavlinks = document.getElementsByClassName('navlinks')[0];
 toggleBurger.addEventListener('click', function()
 {
     toggleNavlinks.classList.toggle('active')
-}
-</script>
+})
