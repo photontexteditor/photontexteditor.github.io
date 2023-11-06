058a22dd5856cbab1e4851d9cb103886ffbc94a8
Ranjit Raut <67329253+theranjitraut@users.noreply.github.com>
2023-11-06 08:46:04 +0545

    additional files are added

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+let toggleBurger = document.getElementsByClassName('burger')[0];
+let toggleNavlinks = document.getElementsByClassName('navlinks')[0];
+toggleBurger.addEventListener('click', function()
+{
+    toggleNavlinks.classList.toggle('active')
+}
+</script>
